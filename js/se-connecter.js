4041be9237b43fedd81cb5e0f759004faeb7cdd5
khirou <khayreddine.mezerreg@gmail.com>
2022-04-27 12:20:29 +0200

    modification

---
+++ js/se-connecter.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+const htmlFooter = `
+<div>
+    <p class="text">
+        ${(new Date()).getFullYear()} - &copy; - Khayreddine - IFOCOP - <a href="mentions.html">Mentions légales</a>
+    </p>
+</div>
+`
+document.querySelector(".mensions-légals").innerHTML=htmlFooter;
